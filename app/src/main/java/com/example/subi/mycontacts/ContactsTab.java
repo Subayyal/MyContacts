28abf7d5ed4b6389c20f674992534c17920e77cf
Subayyal Mustafvi <subayyal.mustafvi@gmail.com>
2017-03-19 19:57:34 -0500

    added contacts anf favorite tabs

---
+++ app/src/main/java/com/example/subi/mycontacts/ContactsTab.java
@@ -0,0 +1,23 @@
+package com.example.subi.mycontacts;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by subi on 3/19/2017.
+ */
+
+public class ContactsTab extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.contacts_tab_main, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/subi/mycontacts/FavoritesTab.java
@@ -0,0 +1,22 @@
+package com.example.subi.mycontacts;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by subi on 3/19/2017.
+ */
+
+public class FavoritesTab extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.favorites_tab_main, container, false);
+        return rootView;
+    }
+}
